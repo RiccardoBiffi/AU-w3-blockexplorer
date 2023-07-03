ddf18220463869658b60ea8df9e2dfe13679a999
Riccardo Biffi <biffi.riccardo91@gmail.com>
2023-07-03 20:54:35 +0200

    Test workflow

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
     getBlockNumber();
   });
 
-  return <div className="App">The block Number: {blockNumber}</div>;
+  return <div className="App">Latest block Number: {blockNumber}</div>;
 }
 
 export default App;
