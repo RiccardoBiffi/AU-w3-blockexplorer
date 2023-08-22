a5155209e077b4a090e76a5f9719912aca4e9564
Riccardo Biffi <biffi.riccardo91@gmail.com>
2023-08-22 12:36:55 +0200

    Updates and styles to BlockInfo e BlockLists

--- src/components/BlockInfo.tsx
+++ src/components/BlockInfo.tsx
@@ -1,67 +1,74 @@
-import * as React from 'react';
-import { Alchemy, Network, Block } from 'alchemy-sdk';
-import { useEffect, useState } from 'react';
+import { useBlock } from '../services';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import styled from '@emotion/styled';
 
 
-const settings = {
-  apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
-  network: Network.ETH_MAINNET,
-};
+export default function BlockInfo(
+  {n, onSelectBlock, isSelected} :
+  { n: number,
+    onSelectBlock: Function, 
+    isSelected: boolean 
+  }) {
+  const block = useBlock(n);
 
-const alchemy = new Alchemy(settings);
-
-
-export default function BlockInfo(props: { n: number }) {
-  const [block, setBlock] = useState<Block | null>();
-
-  useEffect(() => {
-    async function getBlock() {
-      if (props.n === 0) return;
-      setBlock(await alchemy.core.getBlock(props.n));
+  function handleSelectedBlock(n: number) {
+    if (!isSelected) {
+      onSelectBlock(n);
     }
+  }
 
-    getBlock();
-  }, [props.n]);
+  const BlockCard = styled(Card)`
+    margin: 16px;
+    &:hover {
+      box-shadow: 0px 5px 5px -3px rgba(0,0,0,0.2), 0px 8px 10px 1px rgba(0,0,0,0.14), 0px 3px 14px 2px rgba(0,0,0,0.12);
+    }
+  `
 
+  const BlockCardContent = styled(CardContent)`
+    padding: 32px;
+    background-color: ${() => (isSelected ? '#DDDDDD' : '#EEEEEE')};
+    font-size: 24px;
+    border-radius: 4px;
+    color: black;
+
+  `
 
   return (
-    <Card sx={{ maxWidth: 275 }}>
-      <CardContent>
-        <Typography variant="h5" component="div">
-          Block Number: {block?.number}
-        </Typography>
-        <Typography variant="body2">
-          Hash: {block?.hash}
-        </Typography>
-        <Typography variant="body2">
-          Parent Hash: {block?.parentHash}
-        </Typography>
-        <Typography variant="body2">
-          Timestamp: {block?.timestamp ? new Date(block?.timestamp * 1000).toLocaleString() : ""}
-        </Typography>
-        <Typography variant="body2">
-          Nonce: {Number(block?.nonce)}
-        </Typography>
-        <Typography variant="body2">
-          Miner: {block?.miner}
-        </Typography>
-        <Typography variant="body2">
-          Transactions: {block?.transactions.length}
-        </Typography>
-        <Typography variant="body2">
-          Gas Used: {Number(block?.gasUsed)} ({(Number(block?.gasUsed) / Number(block?.gasLimit) * 100).toFixed(2)}%)
-        </Typography>
-        <Typography variant="body2">
-          Gas Limit: {Number(block?.gasLimit)}
-        </Typography>
-        <Typography variant="body2">
-          Difficulty: {Number(block?._difficulty)}
-        </Typography>
-      </CardContent>
-    </Card>
+    <>
+      <BlockCard 
+        sx={{ maxWidth: 350 }} 
+        raised={isSelected}
+        onClick={() => handleSelectedBlock(n)}
+        >
+        <BlockCardContent>
+          <Typography variant="h5" component="div">
+            Block Number: {block?.number}
+          </Typography>
+          <Typography variant="body2">
+            Hash: {block?.hash}
+          </Typography>
+          <Typography variant="body2">
+            Parent Hash: {block?.parentHash}
+          </Typography>
+          <Typography variant="body2">
+            Timestamp: {block?.timestamp ? new Date(block?.timestamp * 1000).toLocaleString() : ""}
+          </Typography>
+          <Typography variant="body2">
+            Miner: {block?.miner}
+          </Typography>
+          <Typography variant="body2">
+            Transactions: {block?.transactions.length}
+          </Typography>
+          <Typography variant="body2">
+            Gas Used: {Number(block?.gasUsed)} ({(Number(block?.gasUsed) / Number(block?.gasLimit) * 100).toFixed(2)}%)
+          </Typography>
+          <Typography variant="body2">
+            Gas Limit: {Number(block?.gasLimit)}
+          </Typography>
+        </BlockCardContent>
+      </BlockCard>
+    </>
   );
 }
--- src/components/BlockList.tsx
+++ src/components/BlockList.tsx
@@ -1,34 +1,31 @@
-import * as React from 'react';
-import { Alchemy, Network, Block } from 'alchemy-sdk';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import BlockInfo from './BlockInfo';
+import Transactions from './Transactions';
 
-const settings = {
-    apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
-    network: Network.ETH_MAINNET,
-};
 
-const alchemy = new Alchemy(settings);
+export default function BlockList({latestBlock}: {latestBlock : number | null}) {
+    const [selectedBlock, setSelectedBlock] = useState<number | null>(null);
 
-export default function BlockList() {
-    const [latestBlock, setlatestBlock] = useState<number | null>();
-
-    useEffect(() => {
-        async function getLatestBlock() {
-            setlatestBlock(await alchemy.core.getBlockNumber());
-        }
-
-        getLatestBlock();
-    });
+    function handleSelectBlock(n: number) {
+        setSelectedBlock(n);
+    }
 
     const blockListArray = new Array(10).fill(0).map((_, i) => {
         const block = latestBlock ? latestBlock - i : 0
-        return <BlockInfo n={block} />
+        return (
+            <BlockInfo
+                key={i}
+                n={block} 
+                onSelectBlock={handleSelectBlock}
+                isSelected={selectedBlock === block}
+            />
+        )
     });
 
     return (
-        <div>
+        <>
             {blockListArray}
-        </div>
+            {selectedBlock && <Transactions blockNumber={selectedBlock} />}
+        </>
     );
 }
