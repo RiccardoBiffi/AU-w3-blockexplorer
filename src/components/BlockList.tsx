caaeb6d117e500a005324835667a49b71f63a19e
Riccardo Biffi <biffi.riccardo91@gmail.com>
2023-09-05 21:30:44 +0200

    Style BlockList

--- src/components/BlockList.tsx
+++ src/components/BlockList.tsx
@@ -1,6 +1,10 @@
 import { useState } from 'react';
 import BlockInfo from './BlockInfo';
 import Transactions from './Transactions';
+import { Stack, Divider } from '@mui/material';
+import styled from '@emotion/styled';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 
 export default function BlockList({latestBlock}: {latestBlock : number | null}) {
@@ -10,7 +14,7 @@ export default function BlockList({latestBlock}: {latestBlock : number | null}) 
         setSelectedBlock(n);
     }
 
-    const blockListArray = new Array(2).fill(0).map((_, i) => {
+    const blockListArray = new Array(3).fill(0).map((_, i) => {
         const block = latestBlock ? latestBlock - i : 0
         return (
             <BlockInfo
@@ -22,9 +26,20 @@ export default function BlockList({latestBlock}: {latestBlock : number | null}) 
         )
     });
 
+    const Icon = styled(FontAwesomeIcon)``
+
     return (
         <>
-            {blockListArray}
+            <h1>Ethereum last 3 blocks</h1>
+            <Stack direction="row-reverse"
+                divider={<Divider><Icon icon={faArrowRight}/></Divider>}
+                spacing={2}
+                justifyContent="center"
+                alignItems="center"
+                my={2}
+                >
+                {blockListArray}
+            </Stack>
             {selectedBlock && <Transactions key={selectedBlock} blockNumber={selectedBlock} />}
         </>
     );
