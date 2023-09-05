307caa724e2bd898f6681bd8a00ea0af2deca75c
Riccardo Biffi <biffi.riccardo91@gmail.com>
2023-09-06 01:01:39 +0200

    Add copy hash on click

--- src/components/fields/Hash.tsx
+++ src/components/fields/Hash.tsx
@@ -1,27 +1,51 @@
 
+import { useState } from 'react';
 import styled from '@emotion/styled';
+import { Snackbar } from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCopy } from '@fortawesome/free-solid-svg-icons';
 
 
 export function Hash({hash}:{ hash: string | undefined }) {
+    const [copy, setCopy] = useState(false);
     const reducedHash = hash?.slice(0, 6) + "..." + hash?.slice(-4);
 
-    const Icon = styled(FontAwesomeIcon)`
+    const Hash = styled.span`
         cursor: pointer;
+        &:hover {
+            text-decoration: underline;
+        }
     `
+
+    const handleClick = (event: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
+        event.stopPropagation();
+        if(hash){
+            navigator.clipboard.writeText(hash);
+            setCopy(true);
+        }
+    }
+
+    const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === 'clickaway') {
+          return;
+        }
+    
+        setCopy(false);
+      };
 
     return (
         <>  
-            <span title={hash}>{reducedHash}&nbsp;</span>
-            <Icon
-                icon={faCopy}
-                onClick={(event) => {
-                    event.stopPropagation();
-                    if(hash)
-                        navigator.clipboard.writeText(hash)
-                    }
-                }
+            <Hash
+                title={hash}
+                onClick={(event) => handleClick(event)}
+                >{reducedHash}&nbsp;<FontAwesomeIcon icon={faCopy}/>
+            </Hash>
+            <Snackbar
+                open={copy}
+                message="Hash copied!"
+                autoHideDuration={2000}
+                onClose={handleClose}
+                anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
             />
         </>
     )
