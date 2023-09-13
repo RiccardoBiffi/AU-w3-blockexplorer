c5fc69b316cc0f872a1256bbde49c7155bbf7b0e
Riccardo Biffi <biffi.riccardo91@gmail.com>
2023-09-13 21:58:04 +0200

    Update transaction table

--- src/components/Transactions.tsx
+++ src/components/Transactions.tsx
@@ -5,33 +5,36 @@ import { DataGrid, GridColDef, GridRowsProp } from '@mui/x-data-grid';
 
 export default function Transactions({blockNumber}:{ blockNumber: number }) {
   const transactions = useTransactions(blockNumber);
+  //todo get eth - eur price from somewhere onchain and add on the table
 
   const rows : GridRowsProp = transactions?.map((t, i) => {
+    const gasPrice = t.gasPrice ? t.gasPrice.toBigInt() : BigInt(0);
+    const gasFee = t.gasLimit.toBigInt() * gasPrice;
+
       return {
         id: t.hash,
         hash: t.hash,
         from: t.from,
         to: t.to,
-        value: Utils.formatEther(t.value.toString()) + " ETH",
+        value: Utils.formatEther(t.value.toString()).substring(0,7) + " ETH",
+        fee: Utils.formatEther(gasFee).substring(0,7) + " ETH",
         gas: t.gasLimit,
         gasPrice: t.gasPrice + " wei",
         nonce: t.nonce
       }
     }) as GridRowsProp;
 
+    //todo fix the width of the columns
   const columns : GridColDef[] = [
-    { field: 'hash', headerName: 'Hash', width: 150 },
-    { field: 'from', headerName: 'From', width:  150 },
+    { field: 'from', headerName: 'From', width: 150 },
     { field: 'to', headerName: 'To', width: 150 },
     { field: 'value', headerName: 'Value sent', width: 250 },
-    { field: 'gas', headerName: 'Gas', width: 100 },
-    { field: 'gasPrice', headerName: 'Gas Price', width: 200 },
-    { field: 'nonce', headerName: 'Nonce', width: 100 },
+    { field: 'fee', headerName: 'Fee', width: 250 },
   ];
 
   return (
     <>
-      {rows ?
+      {rows ? // todo open modal on row click with transaction details
         <DataGrid
           rows={rows}
           columns={columns} 
