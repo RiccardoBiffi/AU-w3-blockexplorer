c278939170161678d71befdd3bbfa14c71e1bb33
Riccardo Biffi <biffi.riccardo91@gmail.com>
2023-09-16 22:44:17 +0200

    Add price context, format files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import BlockList from './components/BlockList';
-import { useLatestBlock } from './services';
-import { Box, Container, Tab, Tabs } from '@mui/material';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faRefresh } from '@fortawesome/free-solid-svg-icons';
+import BlockList from "./components/BlockList";
+import { useLatestBlock } from "./services";
+import { Box, Container, Tab, Tabs } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRefresh } from "@fortawesome/free-solid-svg-icons";
 
-
-import './App.css';
-import styled from '@emotion/styled';
+import "./App.css";
+import styled from "@emotion/styled";
 
 // You can read more Alchemy SDK here:
 // https://docs.alchemy.com/reference/alchemy-sdk-api-surface-overview#api-surface
@@ -22,24 +21,29 @@ function App() {
 
     &:hover {
       transform: rotate(90deg);
-  `
+  `;
 
   return (
     <Container maxWidth="lg">
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
         <Tabs aria-label="basic tabs example" centered>
-          <Tab label="Latest" />
-          <Tab label="Search" />
-          <Tab label="NFTs" />
+          <Tab label="Latest" value={0} />
+          <Tab label="Search" value={1} />
+          <Tab label="NFTs" value={2} />
         </Tabs>
       </Box>
-      <h1>Ethereum last {blockNumber} blocks&nbsp;
-        <Icon title="Click to refresh" onClick={handleRefresh} icon={faRefresh}/>
+      <h1>
+        Ethereum last {blockNumber} blocks&nbsp;
+        <Icon
+          title="Click to refresh"
+          onClick={handleRefresh}
+          icon={faRefresh}
+        />
       </h1>
       <p>Click on a block to see its transactions</p>
       <BlockList latestBlock={latestBlock} number={blockNumber} />
     </Container>
-    );
+  );
 }
 
 export default App;
